9c983727cca9454fcc200d3805ef087b205dfe52
Juan Camilo Londoño <juan2172006@correo.uis.edu.co>
2021-10-21 14:16:45 -0500

    Initial commit

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+using RPG.Core;
+using System;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
+        [SerializeField] float waypointDwellTime = 3f;
+
+        Fighter fighter;
+        Mover mover;
+        Health health;
+        GameObject player;
+
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
+        float timeSinceArrivedWaypoint = Mathf.Infinity;
+        int currentWaypointIndex = 0;
+
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            health = GetComponent<Health>();
+            player = GameObject.FindWithTag("Player");
+            mover = GetComponent<Mover>();
+
+            guardPosition = transform.position;
+        }
+
+        private void Update()
+        {
+
+            if (health.IsDead()) return;
+
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
+            }
+            else
+            {
+                PatrolBehaviour();
+            }
+
+            UpdateTimers();
+        }
+
+        private void UpdateTimers()
+        {
+            timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPosition = guardPosition;
+            if(patrolPath != null)
+            {
+                if (AtWayPoint())
+                {
+                    timeSinceArrivedWaypoint = 0;
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+            if (timeSinceArrivedWaypoint > waypointDwellTime)
+            {
+                mover.StartMoveAction(nextPosition);
+            }
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
+        }
+
+        private void CycleWaypoint()
+        {
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private bool AtWayPoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            timeSinceLastSawPlayer = 0;
+            fighter.Attack(player);
+        }
+
+        private bool InAttackRangeOfPlayer()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            return distanceToPlayer <= chaseDistance;
+        }
+
+        //Called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+
+        NavMeshAgent navMeshAgent;
+
+        Health health;
+
+        private void Start()
+        {
+            health = GetComponent<Health>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            navMeshAgent.enabled = !health.IsDead();
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.isStopped = false;
+            navMeshAgent.destination = destination;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+    }
+}
